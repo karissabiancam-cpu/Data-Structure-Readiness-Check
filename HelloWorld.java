cec678564a399a78828d83e97bb97a6a574dba7d
karissabiancam-cpu <karissabiancam@gmail.com>
2026-07-29 19:16:28 +0800

    Add HelloWorld file

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Dog animal1 = new Dog();
+        Cat animal2 = new Cat();
+        Duck animal3 = new Duck();
+        animal1.sayHello();
+        animal2.sayHello();
+        animal3.sayHello();
+    }
+}
